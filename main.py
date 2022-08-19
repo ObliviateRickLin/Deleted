1dc8fac264d14fd934b554dfc030d8e8df81404b
RickLin <83101411+RickLin616@users.noreply.github.com>
2022-08-19 15:15:23 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,185 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import os
+import numpy as pd
+import joblib
+import pandas as pd
+import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from torch import nn
+from sklearn.preprocessing import *
+
+from ros2df import *
+from tell import *
+
+
+#------------------------------------------------------------
+seed = 7
+torch.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+torch.cuda.manual_seed(seed)
+np.random.seed(seed)
+
+scale = joblib.load("./model/scaler1.pkl")
+net = Net(dim = 6, class_num= 4).cpu()
+model_fp = os.path.join("./model/checkpoint_1700.tar")
+checkpoint = torch.load(model_fp,map_location=torch.device('cpu'))
+net.load_state_dict(checkpoint["net"], strict=False)
+
+#--------------------PAGESTYLE--------------------------------
+
+st.set_page_config(
+     layout="wide"
+ )
+
+#---------------------SIDEBAR----------------------------------
+
+#sb = st.sidebar("")
+with st.sidebar:
+    sc1, sc2 = st.columns((1,2.51))
+    with sc1:
+        st.image("./image/logo1.png")	
+    with sc2:
+        st.title("RISKVIZ TOOL")
+    st.write("    ")
+    st.write("    ")
+    bt1 = st.button("Home Page         " ,key=1)
+    bt2 = st.button("Model Instruction  ",key=2)
+    bt3 = st.button("Risk Visualization" ,key=3)
+    bt4 = st.button("Model Deployment" ,key=4)
+    st.write("    ")
+    st.write("    ")
+    st.write("    ")
+    st.write("    ")
+    st.write("    ")
+    st.write("    ")
+    st.write("    ")
+    st.write("    ")
+    st.write("    ")
+    st.write("    ")
+    st.write("    ")
+    st.write("    ")
+    st.write("    ")
+    st.write("    ")
+    st.write("    ")
+
+    st.sidebar.info(
+        """
+        This web [app](https://ricklin616-riskviz-momenta-stream-try-9ndhvy.streamlitapp.com) is maintained by Jinrui Lin.  
+        Source code: <https://github.com/RickLin616/RiskVIz_Momenta>
+    """
+    )
+ 
+
+
+#----------------Page: Model Instruction---------------------
+if bt2:
+    st.title("Model Instruction")
+    st.header("1.Introduction")
+    st.markdown('''
+        The algorithm aims to develop a method of evaluating real-time driving risk. 
+        It will use TELL, a deep clustering algorithm, to find the risk clustering centers. 
+        The general workflow is composed of four parts:  
+    (1) ROS2DF  
+    (2) Feature Engineering  
+    (3) AutoEncoder (Encoder and Decoder)  
+    (4) TELL  
+    The detailed information will be illustrated in the following sections.    
+    ''')
+    st.header("2.Model Composition")
+    st.subheader("2.1. ROS2DF")
+    st.markdown('''
+    ROS2DF is developed based on the python library rosbag. 
+    It transforms the rosbag file into pandas dataframes and paves way for feature engineering.
+    Note that momenta is replacing rosbag with mfbags. This part will be further developed into MF2DF.
+    ''')    
+    st.subheader("2.2. Feature Engineering")
+    st.markdown('''
+    The current method utilizes surrogate safety metrics (SSMs) to quantify severity of traffic interaction. 
+    Such SSMs are mostly defined to identify safety-critical situations. 
+    We will do feature engineering based on SSMs and incorporate our domain knowledge into our clustering model. 
+    ''')
+    st.subheader("2.3. AutoEncoder")   
+    st.markdown('''
+    The encoder and decoder will both be 3-layers(TBD) fully connected anto-encoder. 
+    It makes feature reduction and decomposition.
+    ''') 
+    st.subheader("2.4. TELL:a continous deep clustering model")
+    
+    st.header("3.Reference")
+    st.markdown('''
+    
+    ''')
+
+#--------------------------PAGE: Risk Visualization-------------------
+elif bt3:    
+    st.title("Risk Visualization")
+    file_path = st.text_input('Enter the path of your rosbag file', '/home/mm/下载/highway/PLEF36110_event_cp_cutin_event_20220530-194448_0.bag')	
+
+    with st.spinner('Wait for it. The neural network is drawing the risk graph...'):
+        df = ros2df(file=file_path)
+        st.table(df.head())
+
+    df["TTC"] = (-df["TTC"]).apply(logistic_transformer)
+    df[["RelativeVelocity_x","RelativeVelocity_y","RelativeHeadingYaw","TTC","C2CRate","U_car"]] = (
+        scale.transform(X=df[["RelativeVelocity_x","RelativeVelocity_y","RelativeHeadingYaw","TTC","C2CRate","U_car"]])
+        )
+    df[["RelativeVelocity_x","RelativeVelocity_y","RelativeHeadingYaw","TTC","C2CRate","U_car"]].to_excel("./temporary_file/indicators_.xlsx")
+    data = DDSafetyDataset(path = "./temporary_file/indicators_.xlsx")
+    data_loader_test = DataLoader(data, batch_size=df.shape[0], shuffle= False, drop_last=True)  # ConcatDataset([train_dataset, test_dataset])
+    feature, labels, hard, soft = inference()
+    soft = soft @ np.array([0,1,2,3])
+    df["soft"] = 3-soft
+    df["hard"] = 3-hard
+
+    col1, col2 = st.columns([3,1])
+    with col1:
+        st.line_chart(df.groupby("TimeStamp")["soft"].max(), width=900, height=380, use_container_width = True)
+    with col2:
+        st.line_chart(df["RelativeVelocity_x"], width=300, height=105, use_container_width = True)
+        st.line_chart(df["RelativeVelocity_y"], width=300, height=105, use_container_width = True)
+        st.line_chart(df["RelativeHeadingYaw"], width=300, height=105, use_container_width = True)    
+#-------------------------PAGE: Model Deployment---------------------------
+elif bt4:
+    st.title("Model Deployment Instruction")
+    st.header("1.Local Deployment")
+    st.markdown('''
+    The following instruction illustrates how to deploy the app on a local computer. 
+    Before moving to specific steps, I highly encourage you to deploy this app in a new virtual environment. 
+
+    After that your can analysis the rosbag file using the local url.
+    The file of this tool is attached and please download it.
+    Then open the terminal of your file and establish the environment according to the requirement.txt with the following code:
+    ''' )
+    st.code('''
+    $pip install -r requirement.txt
+    ''')
+    st.markdown('''
+    After all libararies are installed, run the main.py with the following code.
+    ''')
+    st.code('''
+    $streamlit run ./main.py
+    ''')
+    st.markdown('''
+    Open the link displayed in the terminal and the deployment is finished.
+    ''')
+    st.header("2.Cloud Deployment") 
+    st.markdown('''
+    Depends which type of cloud momenta is using. 
+    The following link might be helpful
+    ''')
+
+
+#--------------------------PAGE: Home Page-------------------------------
+else:
+    col11,col12,col11 = st.columns((0.25,1,0.2))
+    with col12:
+        st.title("DRIVING RISK VISUALIZATION TOOL")
+    col11,col12,col11 = st.columns((0.71,1,0.2))
+    with col12:
+        st.markdown("SAFETY IS THE HIGHEST PRIORITY")
+    st.image("./image/DJI_0372.jpg")
+    
---
+++ tell.py
@@ -0,0 +1,121 @@
+import os
+import joblib
+import pandas as pd
+import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from math import exp
+from torch.utils.data import Dataset, DataLoader
+from torch import nn
+from sklearn.preprocessing import *
+
+class DDSafetyDataset(Dataset):
+  def __init__(self, path):
+    self.path = path #find the corresponding csv file
+    self.df = pd.read_excel(path)
+    return 
+
+  def __getitem__(self, index):
+    df = self.df
+    return np.array(df.iloc[index,1:], dtype = np.float32), 0 #zero will not be used because the algorithm is unsupervised. 
+
+  def __len__(self):
+    return self.df.shape[0]
+
+class Net(nn.Module):
+    def __init__(self, dim, class_num):
+        super(Net, self).__init__()
+        self.class_num = class_num
+        self.encoder = nn.Sequential(
+            nn.Dropout(p=0.2, inplace=False),
+            nn.Linear(dim, 50, bias=True),
+            nn.ReLU(),
+            nn.Dropout(p=0.2, inplace=False),
+            nn.Linear(50, 50, bias=True),
+            nn.ReLU(),
+            nn.Dropout(p=0.2, inplace=False),
+            nn.Linear(50, 20, bias=True),
+            nn.ReLU(),
+            nn.Dropout(p=0.2, inplace=False),
+            nn.Linear(20, 4, bias=True),
+        )
+        self.decoder = nn.Sequential(
+            nn.Linear(4, 20, bias=True),
+            nn.ReLU(),
+            nn.Linear(20, 50, bias=True),
+            nn.ReLU(),
+            nn.Linear(50, 50, bias=True),
+            nn.ReLU(),
+            nn.Linear(50, dim, bias=True),
+            nn.Sigmoid(),
+        )
+        self.cluster_layer = nn.Linear(4, class_num, bias=False)
+        self.cluster_center = torch.rand([class_num, 4], requires_grad=False).cpu()
+
+    def encode(self, x):
+        x = self.encoder(x)
+        x = F.normalize(x)
+        return x
+
+    def decode(self, x):
+        return self.decoder(x)
+
+    def cluster(self, z):
+        return self.cluster_layer(z)
+    
+    def init_cluster_layer(self, alpha, cluster_center):
+        self.cluster_layer.weight.data = 2 * alpha * cluster_center
+
+    def compute_cluster_center(self, alpha):
+        self.cluster_center = 1.0 / (2 * alpha) * self.cluster_layer.weight
+        return self.cluster_center
+
+    def normalize_cluster_center(self, alpha):
+        self.cluster_layer.weight.data = (
+            F.normalize(self.cluster_layer.weight.data, dim=1) * 2.0 * alpha
+        )
+
+    def predict(self, z):
+        distance = torch.cdist(z, self.cluster_center, p=2)
+        prediction = torch.argmin(distance, dim=1)
+        return prediction
+    
+    def predict_(self, z):
+        distance = torch.cdist(z, self.cluster_center, p=2)
+        prediction = F.softmin(distance, dim=1)
+        return prediction
+
+    def set_cluster_centroid(self, mu, cluster_id, alpha):
+        self.cluster_layer.weight.data[cluster_id] = 2 * alpha * mu
+
+
+def inference(): 
+    #net.eval()
+    feature_vector = []
+    labels_vector = []
+    hard_vector = []
+    soft_vector = []
+    with torch.no_grad(): 
+        for step, (x, y) in enumerate(data_loader_test):
+            x = x.cpu()
+            with torch.no_grad():
+                z = net.encode(x)
+                hard = net.predict(z)
+                soft = net.predict_(z)
+            feature_vector.extend(z.detach().cpu().numpy())
+            labels_vector.extend(y.numpy())
+            hard_vector.extend(hard.detach().cpu().numpy())
+            soft_vector.extend(soft.detach().cpu().numpy())
+    feature_vector = np.array(feature_vector)
+    labels_vector = np.array(labels_vector)
+    hard_vector = np.array(hard_vector)
+    soft_vector = np.array(soft_vector)
+    return feature_vector, labels_vector, hard_vector, soft_vector
+
+def logistic_transformer(x):
+    try:
+        y = 1/ (1 + exp(-x))
+    except:
+        y = 0
+    return y
+
